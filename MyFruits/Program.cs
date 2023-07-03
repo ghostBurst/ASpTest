42c9f97d44bb86479fdb4dde9bb1edcb475bc479
ghostBurst <adriencreus59@gmail.comm>
2023-07-04 00:01:52 +0200

    Correction bug envoi mail confrm + New API key SendGrid

--- MyFruits/Program.cs
+++ MyFruits/Program.cs
@@ -1,5 +1,8 @@
-using Microsoft.AspNetCore.Identity;
+using System;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using MyFruits.Areas.Identity.Data;
 using MyFruits.Data;
 using MyFruits.Services;
@@ -7,30 +10,29 @@ using MyFruits.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<MyFruitsUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddRazorPages();
 
-builder.Services.AddSingleton<PathService>();
-builder.Services.AddSingleton<ImageService>();
+// Add email sender service
+builder.Services.AddTransient<IEmailSender, EmailSender>(serviceProvider =>
+    new EmailSender(
+        Environment.GetEnvironmentVariable("SENDGRID_API_KEY"),
+        "vaseline011ze@gmail.com",
+        "Burst"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -43,6 +45,9 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+});
 
 app.Run();
---
+++ MyFruits/Services/EmailSender.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+
+public class EmailSender : IEmailSender
+{
+    private readonly string _apiKey;
+    private readonly string _fromEmail;
+    private readonly string _fromName;
+
+    public EmailSender(string apiKey, string fromEmail, string fromName)
+    {
+        _apiKey = apiKey;
+        _fromEmail = fromEmail;
+        _fromName = fromName;
+    }
+
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        var client = new SendGridClient(_apiKey);
+        var from = new EmailAddress(_fromEmail, _fromName);
+        var to = new EmailAddress(email);
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, null, htmlMessage);
+        return client.SendEmailAsync(msg);
+    }
+}
