e2b8724ec90c3b47bf3358aa79204250638905cf
ghostBurst <adriencreus59@gmail.comm>
2023-07-03 20:06:48 +0200

    Validation mail

--- MyFruits/Data/ApplicationDbContext.cs
+++ MyFruits/Data/ApplicationDbContext.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MyFruits.Areas.Identity.Data;
 using MyFruits.Models;
 
 namespace MyFruits.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<MyFruitsUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<MyFruitsUser> MyFruitsUsers { get; set; }
+
         public DbSet<Fruit> Fruits { get; set; }
         public DbSet<Image> Images { get; set; }
     }
 }
+
+
--- MyFruits/Migrations/20230703180245_AddMyFruitsUser.Designer.cs
+++ MyFruits/Migrations/20230703180245_AddMyFruitsUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyFruits.Data;
 
@@ -11,9 +12,10 @@ using MyFruits.Data;
 namespace MyFruits.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230703180245_AddMyFruitsUser")]
+    partial class AddMyFruitsUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
