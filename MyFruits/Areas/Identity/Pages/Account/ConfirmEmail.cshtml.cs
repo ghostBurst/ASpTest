29b8c7fbcc74a1de445946df17a5ffa4c96d3d35
adrien <adrcr9309@gmail.com>
2023-07-04 11:04:08 +0200

    modif

---
+++ MyFruits/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+using MyFruits.Areas.Identity.Data;
+
+namespace MyFruits.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ConfirmEmailModel : PageModel
+    {
+        private readonly UserManager<MyFruitsUser> _userManager;
+
+        public ConfirmEmailModel(UserManager<MyFruitsUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> OnGetAsync(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{userId}'.");
+            }
+
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            if (!result.Succeeded)
+            {
+                throw new InvalidOperationException($"Error confirming email for user with ID '{userId}':");
+            }
+
+            return Page();
+        }
+    }
+}
